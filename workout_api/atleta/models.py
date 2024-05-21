e9ce96d9ee18f0a941492e4faf6102c77a729a02
Angélica Rafaela <angelmaluh@gmail.com>
2024-05-21 15:27:39 -0300

    Create API FAST

--- workout_api/atleta/models.py
+++ workout_api/atleta/models.py
@@ -1,15 +1,12 @@
 from datetime import datetime
-from sqlalchemy import DateTime, Float, ForeignKey, Integer, String
+from sqlalchemy import DateTime, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from workout_api.contrib.models import BaseModel
-# from workout_api.categorias.models import CategoriaModel
-# from workout_api.centro_treinamento.models import CentroTreinamentoaModel
-
 
 
 class AtletaModel(BaseModel):
-    __tablename__ = 'atletas'
-    
+    __tablename__ = "atletas"
+
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     nome: Mapped[str] = mapped_column(String(50), nullable=False)
     cpf: Mapped[str] = mapped_column(String(11), unique=True, nullable=False)
@@ -18,7 +15,13 @@ class AtletaModel(BaseModel):
     altura: Mapped[float] = mapped_column(Float, nullable=False)
     sexo: Mapped[str] = mapped_column(String(1), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-    categoria: Mapped['CategoriaModel'] = relationship(back_populates='atleta')
-    categoria_id: Mapped[int] = mapped_column(ForeignKey('categorias.pk_id'))
-    centro_treinamento: Mapped['CentroTreinamentoaModel'] = relationship(back_populates='atleta')
-    centro_treinamento_id: Mapped[int] = mapped_column(ForeignKey('centro_treinamento.pk_id'))
+    categoria: Mapped["CategoriaModel"] = relationship(
+        back_populates="atleta", lazy="selectin"
+    )
+    categoria_id: Mapped[int] = mapped_column(ForeignKey("categorias.pk_id"))
+    centro_treinamento: Mapped["CentroTreinamentoModel"] = relationship(
+        back_populates="atleta", lazy="selectin"
+    )
+    centro_treinamento_id: Mapped[int] = mapped_column(
+        ForeignKey("centros_treinamento.pk_id")
+    )
